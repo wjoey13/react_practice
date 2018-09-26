b406dd1cde3e306bce312c263d599ffa09700102
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-09-25 21:26:52 -0600

    required express, cors, and body-parser. Then created a port

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 30313;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`wanna know what port is active? It's port ${port}`);
+});
+
+app.get('/', (req, res, next) =>   {
+})
